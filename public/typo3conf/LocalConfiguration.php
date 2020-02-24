584563b73f06309ee886e2bf4b883af28a77abe7
Thomas Kieslich <post@thomaskieslich.de>
2020-02-24 21:17:50 +0100

    update to TYPO3 10

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -18,15 +18,12 @@ return [
             ],
         ],
     ],
-    'EXT' => [
-        'extConf' => [
-            'backend' => 'a:6:{s:9:"loginLogo";s:0:"";s:19:"loginHighlightColor";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:11:"backendLogo";s:0:"";s:14:"backendFavicon";s:0:"";}',
-            'bootstrap_package' => 'a:8:{s:16:"disablePageTsRTE";s:1:"0";s:27:"disablePageTsBackendLayouts";s:1:"0";s:20:"disablePageTsTCEMAIN";s:1:"0";s:20:"disablePageTsTCEFORM";s:1:"0";s:28:"disablePageTsContentElements";s:1:"0";s:20:"disableCssProcessing";s:1:"0";s:24:"disableGoogleFontCaching";s:1:"0";s:17:"disableFontLoader";s:1:"0";}',
-            'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
-            'indexed_search' => 'a:20:{s:8:"pdftools";s:9:"/usr/bin/";s:8:"pdf_mode";s:2:"20";s:5:"unzip";s:9:"/usr/bin/";s:6:"catdoc";s:9:"/usr/bin/";s:6:"xlhtml";s:9:"/usr/bin/";s:7:"ppthtml";s:9:"/usr/bin/";s:5:"unrtf";s:9:"/usr/bin/";s:18:"trackIpInStatistic";s:1:"2";s:9:"debugMode";s:1:"0";s:18:"fullTextDataLength";s:1:"0";s:23:"disableFrontendIndexing";s:1:"0";s:21:"enableMetaphoneSearch";s:1:"1";s:6:"minAge";s:2:"24";s:6:"maxAge";s:1:"0";s:16:"maxExternalFiles";s:1:"5";s:26:"useCrawlerForExternalFiles";s:1:"0";s:11:"flagBitMask";s:3:"192";s:16:"ignoreExtensions";s:0:"";s:17:"indexExternalURLs";s:1:"0";s:16:"useMysqlFulltext";s:1:"0";}',
-            'scheduler' => 'a:2:{s:11:"maxLifetime";s:4:"1440";s:15:"showSampleTasks";s:1:"1";}',
-            'solr' => 'a:5:{s:35:"useConfigurationFromClosestTemplate";s:1:"0";s:43:"useConfigurationTrackRecordsOutsideSiteroot";s:1:"1";s:29:"useConfigurationMonitorTables";s:0:"";s:27:"allowSelfSignedCertificates";s:1:"0";s:19:"allowLegacySiteMode";s:1:"0";}',
-            'translate_locallang' => 'a:10:{s:14:"defaultLangKey";s:2:"en";s:8:"langKeys";s:8:"de,fr,it";s:7:"useL10n";s:1:"0";s:10:"clearCache";s:1:"0";s:5:"debug";s:1:"0";s:9:"extFilter";s:1:"*";s:11:"allowedExts";s:0:"";s:12:"allowedFiles";s:0:"";s:17:"modifyDefaultLang";s:1:"0";s:10:"modifyKeys";s:1:"0";}',
+    'EXT' => [],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'de',
+            ],
         ],
     ],
     'EXTENSIONS' => [
@@ -78,25 +75,6 @@ return [
             'maxLifetime' => '1440',
             'showSampleTasks' => '1',
         ],
-        'solr' => [
-            'allowLegacySiteMode' => '0',
-            'allowSelfSignedCertificates' => '0',
-            'useConfigurationFromClosestTemplate' => '0',
-            'useConfigurationMonitorTables' => '',
-            'useConfigurationTrackRecordsOutsideSiteroot' => '1',
-        ],
-        'translate_locallang' => [
-            'allowedExts' => '',
-            'allowedFiles' => '',
-            'clearCache' => '0',
-            'debug' => '0',
-            'defaultLangKey' => 'en',
-            'extFilter' => '*',
-            'langKeys' => 'de,fr,it',
-            'modifyDefaultLang' => '0',
-            'modifyKeys' => '0',
-            'useL10n' => '0',
-        ],
     ],
     'FE' => [
         'debug' => true,
@@ -132,9 +110,8 @@ return [
             'unifiedPageTranslationHandling' => true,
         ],
         'sitename' => 'New TYPO3 site',
-        'systemLogLevel' => 0,
         'systemMaintainers' => [
-            3,
+            1,
         ],
     ],
 ];
